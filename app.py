963c1f156905aa8e1a4e6ddb93e9d587ca794b6d
Htbibalan <103408455+Htbibalan@users.noreply.github.com>
2025-02-27 15:56:54 +0100

    Added /health check route

--- app.py
+++ app.py
@@ -60,6 +60,9 @@ def distribute():
     mice_data.clear()
 
     return jsonify({"groups": groups, "weights": group_sums})
+@app.route('/health')
+def health_check():
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
